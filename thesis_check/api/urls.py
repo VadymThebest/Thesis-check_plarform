f870d76c0b7be9ec8ce70663b8ce8fffbc44a221
VadymThebest <vadymgrigoriev005@gmail.com>
2025-12-04 21:52:23 +0200

    Cleaning everything from the branch for the new workspace

--- thesis_check/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UploadThesisView, ThesisResultView
-
-urlpatterns = [
-    path('upload/', UploadThesisView.as_view(), name='upload'),
-    path('check/<int:id>/', ThesisResultView.as_view(), name='check'),
-]
